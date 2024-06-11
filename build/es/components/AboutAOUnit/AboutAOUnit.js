391ac758c8759429d12c6049273dff5010e6d7ea
Jen Jones Arnesen <jennifer@dhis2.org>
2024-06-11 08:55:27 +0000

    6014600 chore: add comment explaining folder name

--- build/es/components/AboutAOUnit/AboutAOUnit.js
+++ build/es/components/AboutAOUnit/AboutAOUnit.js
@@ -55,7 +55,7 @@ const AboutAOUnit = /*#__PURE__*/forwardRef((_ref3, ref) => {
     fromServerDate
   } = useTimeZoneConversion();
   const queries = useMemo(() => getQueries(type), [type]);
-  console.log('testing build from d2-ci 2');
+  console.log('testing build from d2-ci 3');
   const {
     data,
     loading: dataIsLoading,
