0f47934a1ec551c0cb50fb7be126f3d9612a04cf
Edoardo Sabadelli <edoardo@dhis2.org>
2024-08-05 09:06:13 +0000

    1615bb4 chore: use named functions for default exports too

--- build/cjs/modules/visTypes.js
+++ build/cjs/modules/visTypes.js
@@ -83,7 +83,7 @@ const twoCategoryChartTypes = [VIS_TYPE_COLUMN, VIS_TYPE_STACKED_COLUMN, VIS_TYP
 const columnBasedTypes = [VIS_TYPE_COLUMN, VIS_TYPE_BAR, VIS_TYPE_YEAR_OVER_YEAR_COLUMN, VIS_TYPE_STACKED_COLUMN, VIS_TYPE_STACKED_BAR];
 const verticalTypes = [VIS_TYPE_BAR, VIS_TYPE_STACKED_BAR, VIS_TYPE_GAUGE];
 const legendSetTypes = [VIS_TYPE_COLUMN, VIS_TYPE_BAR, VIS_TYPE_GAUGE, VIS_TYPE_SINGLE_VALUE, VIS_TYPE_PIVOT_TABLE, VIS_TYPE_STACKED_COLUMN, VIS_TYPE_STACKED_BAR];
-const defaultVisType = exports.defaultVisType = VIS_TYPE_COLUMN;
+const defaultVisType = exports.defaultVisType = VIS_TYPE_PIVOT_TABLE;
 const isStacked = type => stackedTypes.includes(type);
 exports.isStacked = isStacked;
 const isYearOverYear = type => yearOverYearTypes.includes(type);
