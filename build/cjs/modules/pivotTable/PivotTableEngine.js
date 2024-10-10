f37faeae7899db1e91c592396ba52dc5bb8ca757
Edoardo Sabadelli <edoardo@dhis2.org>
2024-10-10 08:51:25 +0000

    82a10b0 refactor: replace ||= operator, not transformed by Babel

--- build/cjs/modules/pivotTable/PivotTableEngine.js
+++ build/cjs/modules/pivotTable/PivotTableEngine.js
@@ -793,7 +793,8 @@ class PivotTableEngine {
           // accumulating other value types like text values does not make sense
           if ((0, _valueTypes.isCumulativeValueType)(valueType)) {
             // initialise to 0 for cumulative types
-            acc ||= 0;
+            // (||= is not transformed correctly in Babel with the current setup)
+            acc || (acc = 0);
             if (this.data[row] && this.data[row][column]) {
               const dataRow = this.data[row][column];
               const rawValue = cellType === _pivotTableConstants.CELL_TYPE_VALUE ? dataRow[this.dimensionLookup.dataHeaders.value] : dataRow.value;
