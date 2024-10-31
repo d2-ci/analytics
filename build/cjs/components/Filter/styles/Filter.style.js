512977e849d802f5c9f9424c2b62b0615956e911
GitHub <noreply@github.com>
2024-10-31 13:17:53 +0000

    2e750e3 Merge branch 'master' into dependabot/npm_and_yarn/master/fs-extra-11.2.0

---
+++ build/cjs/components/Filter/styles/Filter.style.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _ui = require("@dhis2/ui");
+const _defaultExport = [`.container.jsx-2445670963{margin-bottom:${_ui.spacers.dp16};}`];
+_defaultExport.__hash = "2445670963";
+var _default = _defaultExport;
+exports.default = _default;
---
+++ build/es/components/Interpretations/common/Interpretation/InterpretationSharingLink.js
@@ -0,0 +1,31 @@
+import _JSXStyle from "styled-jsx/style";
+import i18n from '@dhis2/d2-i18n';
+import { SharingDialog, colors, spacers } from '@dhis2/ui';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+const InterpretationSharingLink = _ref => {
+  let {
+    type,
+    id
+  } = _ref;
+  const [showSharingDialog, setShowSharingDialog] = useState(false);
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("div", {
+    className: _JSXStyle.dynamic([["3990725326", [spacers.dp4, spacers.dp8, colors.grey800]]]) + " " + "container"
+  }, /*#__PURE__*/React.createElement("span", {
+    onClick: () => setShowSharingDialog(true),
+    className: _JSXStyle.dynamic([["3990725326", [spacers.dp4, spacers.dp8, colors.grey800]]]) + " " + "link"
+  }, i18n.t('Manage sharing'))), showSharingDialog && /*#__PURE__*/React.createElement(SharingDialog, {
+    open: true,
+    type: type,
+    id: id,
+    onClose: () => setShowSharingDialog(false)
+  }), /*#__PURE__*/React.createElement(_JSXStyle, {
+    id: "3990725326",
+    dynamic: [spacers.dp4, spacers.dp8, colors.grey800]
+  }, [`.container.__jsx-style-dynamic-selector{display:-webkit-box;display:-webkit-flex;display:-ms-flexbox;display:flex;-webkit-align-items:center;-webkit-box-align:center;-ms-flex-align:center;align-items:center;-webkit-box-pack:end;-webkit-justify-content:flex-end;-ms-flex-pack:end;justify-content:flex-end;gap:${spacers.dp4};margin-top:${spacers.dp8};font-size:13px;color:${colors.grey800};cursor:pointer;}`, ".link.__jsx-style-dynamic-selector{-webkit-text-decoration:underline;text-decoration:underline;}"]));
+};
+InterpretationSharingLink.propTypes = {
+  id: PropTypes.string,
+  type: PropTypes.string
+};
+export { InterpretationSharingLink };
