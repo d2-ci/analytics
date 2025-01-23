949b16adae37a94768847d936760657b3a95fee0
Jen Jones Arnesen <jennifer@dhis2.org>
2025-01-23 17:52:34 +0000

    66e3411 chore: more debugging

--- build/cjs/modules/pivotTable/PivotTableEngine.js
+++ build/cjs/modules/pivotTable/PivotTableEngine.js
@@ -430,9 +430,13 @@ class PivotTableEngine {
   }
   columnIsEmpty(column) {
     return !this.rowMap.some(row => {
+      var _this$data$row;
       console.log('jj columnIsEmpty', {
         row,
-        column
+        column,
+        data: this.data,
+        rowdata: this.data[row],
+        colData: (_this$data$row = this.data[row]) === null || _this$data$row === void 0 ? void 0 : _this$data$row[column]
       });
       return this.data[row][column];
     });
