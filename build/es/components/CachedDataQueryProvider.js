32b07a4a7ec182faa661e65c55c7375f07a8baf2
Jen Jones Arnesen <jennifer@dhis2.org>
2025-02-17 12:18:29 +0000

    2a16dda chore: add workflow that doesnt succeed until PR tasks completed

---
+++ build/es/components/CachedDataQueryProvider.js
@@ -0,0 +1,46 @@
+import { useDataQuery } from '@dhis2/app-runtime';
+import i18n from '@dhis2/d2-i18n';
+import { Layer, CenteredContent, CircularLoader, NoticeBox } from '@dhis2/ui';
+import PropTypes from 'prop-types';
+import React, { createContext, useContext } from 'react';
+const CachedDataQueryCtx = /*#__PURE__*/createContext({});
+const CachedDataQueryProvider = _ref => {
+  let {
+    query,
+    dataTransformation,
+    children,
+    translucent = true
+  } = _ref;
+  const {
+    data: rawData,
+    ...rest
+  } = useDataQuery(query);
+  const {
+    error,
+    loading
+  } = rest;
+  const data = rawData && dataTransformation ? dataTransformation(rawData) : rawData;
+  if (loading) {
+    return /*#__PURE__*/React.createElement(Layer, {
+      translucent: translucent
+    }, /*#__PURE__*/React.createElement(CenteredContent, null, /*#__PURE__*/React.createElement(CircularLoader, null)));
+  }
+  if (error) {
+    const fallbackMsg = i18n.t('This app could not retrieve required data.');
+    return /*#__PURE__*/React.createElement(NoticeBox, {
+      error: true,
+      title: i18n.t('Network error')
+    }, error.message || fallbackMsg);
+  }
+  return /*#__PURE__*/React.createElement(CachedDataQueryCtx.Provider, {
+    value: data
+  }, children);
+};
+CachedDataQueryProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+  query: PropTypes.object.isRequired,
+  dataTransformation: PropTypes.func,
+  translucent: PropTypes.bool
+};
+const useCachedDataQuery = () => useContext(CachedDataQueryCtx);
+export { CachedDataQueryProvider, useCachedDataQuery };
