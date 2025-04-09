60ef761f910363a7f2ba4cad805a2272387ec205
HendrikThePendric <hendrik@dhis2.org>
2025-04-09 12:25:59 +0000

    5fd4093 fix: disable boost mode while exporting scatter plots

--- build/cjs/visualizations/config/adapters/dhis_highcharts/exporting.js
+++ build/cjs/visualizations/config/adapters/dhis_highcharts/exporting.js
@@ -7,15 +7,22 @@ exports.default = getExporting;
 var _visTypes = require("../../../../modules/visTypes.js");
 var _index = _interopRequireDefault(require("./events/loadCustomSVG/singleValue/index.js"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+const BASE_EXPORTING_CONFIG = {
+  // disable exporting context menu
+  enabled: false,
+  // use offline exporting only
+  fallbackToExportServer: false,
+  allowHTML: true,
+  showExportInProgress: true,
+  sourceHeight: 768,
+  sourceWidth: 1024,
+  scale: 1
+};
 function getExporting(visType) {
-  const exporting = {
-    // disable exporting context menu
-    enabled: false
-  };
   switch (visType) {
     case _visTypes.VIS_TYPE_SINGLE_VALUE:
       return {
-        ...exporting,
+        ...BASE_EXPORTING_CONFIG,
         chartOptions: {
           chart: {
             events: {
@@ -24,7 +31,17 @@ function getExporting(visType) {
           }
         }
       };
+    // This is a workaround for https://github.com/highcharts/highcharts/issues/8333
+    case _visTypes.VIS_TYPE_SCATTER:
+      return {
+        ...BASE_EXPORTING_CONFIG,
+        chartOptions: {
+          boost: {
+            enabled: false
+          }
+        }
+      };
     default:
-      return exporting;
+      return BASE_EXPORTING_CONFIG;
   }
 }
