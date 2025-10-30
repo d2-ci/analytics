bfb3931f54914ffd934540e67ba9cf01838c8991
HendrikThePendric <hendrik@dhis2.org>
2025-10-30 09:24:43 +0000

    8f51e67 fix: prevent crash when cancelling comment edit

---
+++ build/es/modules/layout/__tests__/layoutGetDimension.spec.js
@@ -0,0 +1,8 @@
+import { DIMENSION_PROP_ID } from '../dimension.js';
+import { layoutGetDimension } from '../layoutGetDimension.js';
+import { TEST_LAYOUT, TEST_DIMENSION_1 } from '../testResources.js';
+describe('layoutGetDimension', () => {
+  it('should return true if the dimension id is found in the layout, otherwise false', () => {
+    expect(layoutGetDimension(TEST_LAYOUT, TEST_DIMENSION_1[DIMENSION_PROP_ID.name])).toEqual(TEST_DIMENSION_1);
+  });
+});
