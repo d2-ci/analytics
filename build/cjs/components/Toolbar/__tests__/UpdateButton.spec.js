a842331c4d86c0ebe21bd68d02a35ea550cc0626
Edoardo Sabadelli <edoardo@dhis2.org>
2025-10-30 09:39:00 +0000

    858e647 fix: read displayProperty from currentUser first

---
+++ build/cjs/components/Toolbar/__tests__/UpdateButton.spec.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var _react = require("@testing-library/react");
+var _userEvent = _interopRequireDefault(require("@testing-library/user-event"));
+var _react2 = _interopRequireDefault(require("react"));
+var _index = require("../index.js");
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
+describe('<UpdateButton/>', () => {
+  const noop = () => {};
+  test('accepts an `onClick` prop', async () => {
+    const user = _userEvent.default.setup();
+    const onClick = jest.fn();
+    (0, _react.render)(/*#__PURE__*/_react2.default.createElement(_index.UpdateButton, {
+      onClick: onClick
+    }));
+    await user.click(_react.screen.getByRole('button'));
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+  test('accepts a `dataTest` prop', () => {
+    const dataTest = 'test';
+    (0, _react.render)(/*#__PURE__*/_react2.default.createElement(_index.UpdateButton, {
+      onClick: noop,
+      dataTest: dataTest
+    }));
+    expect(_react.screen.getByTestId(dataTest)).toBeInTheDocument();
+  });
+  test('accepts a `disabled` prop', () => {
+    (0, _react.render)(/*#__PURE__*/_react2.default.createElement(_index.UpdateButton, {
+      disabled: true,
+      onClick: noop
+    }));
+    expect(_react.screen.getByRole('button')).toBeDisabled();
+  });
+  test('accepts an `loading` prop', () => {
+    (0, _react.render)(/*#__PURE__*/_react2.default.createElement(_index.UpdateButton, {
+      onClick: noop,
+      loading: true
+    }));
+    expect(_react.screen.getByTestId('dhis2-uicore-circularloader')).toBeInTheDocument();
+  });
+});
---
+++ build/es/modules/layout/layoutGetAllItems.js
@@ -0,0 +1,6 @@
+import { axisGetAllItems } from './axisGetAllItems.js';
+import { layoutGetAllAxes } from './layoutGetAllAxes.js';
+export const layoutGetAllItems = layout => layoutGetAllAxes(layout).reduce((allItems, axis) => {
+  allItems.push(...axisGetAllItems(axis));
+  return allItems;
+}, []);
