6222181bc7e3ebe5b4e26a19ec0b596e9b568a97
Jan Henrik Øverland <janhenrik.overland@gmail.com>
2026-03-10 09:44:36 +0000

    c82a44f fix: export

---
+++ build/es/components/Toolbar/UpdateButton.js
@@ -0,0 +1,30 @@
+import _JSXStyle from "styled-jsx/style";
+import i18n from '@dhis2/d2-i18n';
+import { CircularLoader, IconSync16, colors } from '@dhis2/ui';
+import PropTypes from 'prop-types';
+import React from 'react';
+import menuButtonStyles from './MenuButton.styles.js';
+export const UpdateButton = ({
+  onClick,
+  disabled,
+  loading,
+  dataTest = 'dhis2-analytics-updatebutton'
+}) => /*#__PURE__*/React.createElement("button", {
+  onClick: onClick,
+  disabled: disabled,
+  "data-test": dataTest,
+  className: `jsx-${menuButtonStyles.__hash}` + " " + _JSXStyle.dynamic([["2364287882", [colors.blue700, colors.blue100, colors.blue200]]])
+}, loading ? /*#__PURE__*/React.createElement(CircularLoader, {
+  extrasmall: true
+}) : /*#__PURE__*/React.createElement(IconSync16, null), i18n.t('Update'), /*#__PURE__*/React.createElement(_JSXStyle, {
+  id: menuButtonStyles.__hash
+}, menuButtonStyles), /*#__PURE__*/React.createElement(_JSXStyle, {
+  id: "2364287882",
+  dynamic: [colors.blue700, colors.blue100, colors.blue200]
+}, [`button.__jsx-style-dynamic-selector{gap:8px;color:${colors.blue700};font-weight:500;}`, `button.__jsx-style-dynamic-selector:hover.__jsx-style-dynamic-selector:enabled{background:${colors.blue100};}`, `button.__jsx-style-dynamic-selector:active{background:${colors.blue200};}`]));
+UpdateButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  dataTest: PropTypes.string,
+  disabled: PropTypes.bool,
+  loading: PropTypes.bool
+};
